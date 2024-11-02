f1f11f7b08703c430498c58d91407873d02d0136
Artjem Nikitin <a.nikitin@space307.com>
2024-11-02 22:52:33 +0100

    add relative links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
+import relativeLinks from "astro-relative-links";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dontshoot11.github.io",
   build: {
     assets: "assets",
   },
-  integrations: [svelte()],
+  integrations: [svelte(), relativeLinks()],
 });
