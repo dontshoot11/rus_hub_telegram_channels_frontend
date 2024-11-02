a3d51340793cdac03737296de0135cdda7283cb6
Artjem Nikitin <a.nikitin@space307.com>
2024-11-02 22:44:52 +0100

    add site to config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  site: "https://dontshoot11.github.io",
+  integrations: [svelte()],
 });
